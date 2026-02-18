8639a9c2eaef3b0036d81c36e552019a1ad784ed
Khalil Abu-Dahab <khalil.abudahab@cgps.group>
2026-02-18 18:26:15 +0000

    Revert "Update update.js"
    This reverts commit 6b11d6d9a8bd3c0b70db371d24b7175faa4a000b.

--- pages/api/cron/update.js
+++ pages/api/cron/update.js
@@ -21,35 +21,19 @@ export default async function handler(req, res) {
 
   const projectsCount = await db.collection("projects").count({ numEntries: { $exists: false } });
   const projectsCursor = db.collection("projects").find({ numEntries: { $exists: false } });
-  
-  const updateProject = async (doc, currentIndex) => {
-    console.info("Updating project %s / %s. %s \r", currentIndex, projectsCount, doc.id);
+  for await (const doc of projectsCursor) {
+    console.info("Updating project %s / %s. %s \r", index, projectsCount, doc.id);
     try {
       const jsonDocument = await ProjectsService.toViewerJson(doc);
+
       const numEntries = await countEntries(jsonDocument);
+
       await db.collection("projects").updateOne({ _id: doc._id }, { $set: { numEntries } });
     }
     catch (err) {
       console.error("Error updating project %s: %s", doc.id, err);
     }
-  };
-
-  const concurrencyLimit = 4;
-  const batch = [];
-  
-  for await (const doc of projectsCursor) {
-    batch.push(updateProject(doc, index));
     index += 1;
-    
-    if (batch.length >= concurrencyLimit) {
-      await Promise.all(batch);
-      batch.length = 0;
-    }
-  }
-  
-  // Process remaining items
-  if (batch.length > 0) {
-    await Promise.all(batch);
   }
 
   return res.status(200).json({ success: true });
