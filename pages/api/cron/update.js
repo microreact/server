b2c36652c9a6c4816201405a34b86ccc722fc080
Khalil Abu-Dahab <khalil.abudahab@cgps.group>
2026-02-19 09:45:05 +0000

    Update update.js

--- pages/api/cron/update.js
+++ pages/api/cron/update.js
@@ -52,6 +52,7 @@ export default async function handler(req, res) {
         }
         catch (err) {
           console.error("Error updating project %s: %s", doc.id, err);
+          await db.collection("projects").updateOne({ _id: doc._id }, { $set: { numEntries: 0 } });
         }
       },
     );
